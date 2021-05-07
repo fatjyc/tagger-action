3813dc4e91671ceb3c261d93cce8b17ba9328f0b
jiong <fatjyc@gmail.com>
2021-05-07 11:28:05 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,26 @@
 import * as core from '@actions/core'
-import {Octokit} from '@octokit/core'
+import {Octokit} from '@octokit/rest'
 
 async function run(): Promise<void> {
-  try {
-    const owner = core.getInput('owner', {required: true})
-    const repo = core.getInput('repo', {required: true})
-    const token = core.getInput('token', {required: true})
-    const tag = core.getInput('tag', {required: true})
-    const ref = core.getInput('ref', {required: true})
+  const owner = core.getInput('owner', {required: true})
+  const repo = core.getInput('repo', {required: true})
+  const token = core.getInput('token', {required: true})
+  const tag = core.getInput('tag', {required: true})
+  const ref = core.getInput('ref', {required: true})
 
-    const octokit = new Octokit({auth: token})
+  const octokit = new Octokit({auth: token})
 
-    const result = await octokit.request(
-      'GET /repos/{owner}/{repo}/git/ref/{ref}',
-      {
-        owner,
-        repo,
-        ref: `heads/${ref}`
-      }
-    )
-
-    let sha = ref
-
-    if (
-      result.status === 200 &&
-      result.data.ref &&
-      result.data.object.type === 'commit'
-    ) {
-      sha = result.data.object.sha
-    }
-
-    await octokit.request('POST /repos/{owner}/{repo}/git/refs', {
-      owner,
-      repo,
-      ref: `refs/tags/${tag}`,
-      sha
+  octokit.rest.git
+    .getRef({owner, repo, ref})
+    .then(value => {
+      const sha = value.data.object.sha
+      octokit.rest.git
+        .createRef({owner, repo, ref: `refs/tags/${tag}`, sha})
+        .catch(error => console.error('create ref error, ', error))
     })
-  } catch (error) {
-    core.error(error)
-    core.setFailed(error.message)
-  }
+    .catch(error => {
+      console.error('get ref error, ', error)
+    })
 }
 
 run()
